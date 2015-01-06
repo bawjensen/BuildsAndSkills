06cb4fd4e258bf19b360e23badf1ef1bd0bfc464
Bryan Jensen <bryanawjensen@gmail.com>
2015-01-05 19:43:02 -0800

    Trying more drastic changes

--- web.js
+++ web.js
@@ -3,15 +3,16 @@ var bodyParser  = require("body-parser"),
     logfmt      = require("logfmt");
 
 var app = express();
+
 // Server defaults to port 7500
 app.set('port', (process.env.PORT || 5000));
 
 // Static serving files from specific folders
-app.use('/foundation',  express.static(__dirname + '/foundation'));
-app.use('/css',         express.static(__dirname + '/css'));
-app.use('/js',          express.static(__dirname + '/js'));
-app.use('/images',      express.static(__dirname + '/images'));
-app.use('/static',      express.static(__dirname + '/static'));
+// app.use('/foundation',  express.static(__dirname + '/foundation'));
+// app.use('/css',         express.static(__dirname + '/css'));
+// app.use('/js',          express.static(__dirname + '/js'));
+// app.use('/images',      express.static(__dirname + '/images'));
+// app.use('/static',      express.static(__dirname + '/static'));
 
 // Other stuff to use
 app.use(logfmt.requestLogger());
@@ -24,5 +25,5 @@ app.get('/', function(req, res) {
 
 // Start up the server
 app.listen(app.get('port'), function() {
-    console.log("Listening on " + app.get('port'));
+    console.log("Active!");
 });
