81b9e6f797968491345b1be67e928ca376818bba
Bryan Jensen <bryanawjensen@gmail.com>
2015-01-05 20:37:31 -0800

    Done for the night - gotta make heroku do es6

---
+++ README.md
@@ -0,0 +1,9 @@
+# LoLRunes
+
+Order of events:
+
++ Get all challengers
++ Get match history for each
++ For each match, get champ, runes and masteries
++ Parse masteries, runes and find most popular
++ Assign that the associated champ
--- web.js
+++ web.js
@@ -1,7 +1,10 @@
 var bodyParser  = require("body-parser"),
     express     = require("express"),
     logfmt      = require("logfmt"),
-    request     = require("request");
+    request     = require("request")
+    querystring = require("querystring");
+
+var key = '19deda21-b9ca-40d3-af2c-5037a30b37b9';
 
 var app = express();
 
@@ -9,38 +12,61 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 // Static serving files from specific folders
-// app.use('/foundation',  express.static(__dirname + '/foundation'));
 // app.use('/css',         express.static(__dirname + '/css'));
 // app.use('/js',          express.static(__dirname + '/js'));
 // app.use('/images',      express.static(__dirname + '/images'));
-// app.use('/static',      express.static(__dirname + '/static'));
 
 // Other stuff to use
 app.use(logfmt.requestLogger());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.get('/', function(req, res) {
-    request.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=19deda21-b9ca-40d3-af2c-5037a30b37b9', function handleResp(err, resp, body) {
+    var url = 'https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?';
+    var query  = {
+        type: 'RANKED_SOLO_5x5',
+        api_key: key
+    }
+    var queryUrl = url + querystring.stringify(query);
+
+    request.get(queryUrl, function handleResp(err, resp, body) {
         if (err) {
-            console.log(err);
-            res.send("No");
+            res.send(err);
         }
         else {
-            var champs = JSON.parse(body).data;
 
-            dataBuffer = [];
-
-            for (champKey in champs) {
-                var champ = champs[champKey];
-
-                dataBuffer.push( { name: champ.name, image: 'http://ddragon.leagueoflegends.com/cdn/4.2.6/img/champion/' + champ.image.full } );
-            }
-
-            res.render('index.jade', { champs: dataBuffer });
         }
     });
-    // res.render('index.jade');
+
+
+    // var url = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?';
+    // var query = {
+    //     champData: 'image',
+    //     api_key: key
+    // };
+    // var queryUrl = url + querystring.stringify(query);
+    
+    // request.get(queryUrl, function handleResp(err, resp, body) {
+    //     if (err) {
+    //         console.log(err);
+    //         res.send("No");
+    //     }
+    //     else {
+    //         var champs = JSON.parse(body).data;
+
+    //         dataBuffer = [];
+
+    //         for (champKey in champs) {
+    //             var champ = champs[champKey];
+
+    //             dataBuffer.push( { name: champ.name, image: 'http://ddragon.leagueoflegends.com/cdn/4.2.6/img/champion/' + champ.image.full } );
+    //         }
+
+    //         res.render('index.jade', { champs: dataBuffer });
+    //     }
+
+    // });
 });
 
 // Start up the server
