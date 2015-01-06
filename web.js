4287bf118512df5f6baf020009f87517931379d7
Bryan Jensen <bryanawjensen@gmail.com>
2015-01-05 20:11:57 -0800

    Got names and pictures showing

--- web.js
+++ web.js
@@ -21,7 +21,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
-    res.render('index.jade');
+    request.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=19deda21-b9ca-40d3-af2c-5037a30b37b9', function handleResp(err, resp, body) {
+        if (err) {
+            console.log(err);
+            res.send("No");
+        }
+        else {
+            var champs = JSON.parse(body).data;
+
+            dataBuffer = [];
+
+            for (champKey in champs) {
+                var champ = champs[champKey];
+
+                dataBuffer.push( { name: champ.name, image: 'http://ddragon.leagueoflegends.com/cdn/4.2.6/img/champion/' + champ.image.full } );
+            }
+
+            res.render('index.jade', { champs: dataBuffer });
+        }
+    });
+    // res.render('index.jade');
 });
 
 // Start up the server
