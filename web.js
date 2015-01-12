b244e62cca54100ca3b7794bd288b7d00a851c29
Bryan Jensen <bryanawjensen@gmail.com>
2015-01-11 21:52:45 -0500

    Got list of challenger ids

--- web.js
+++ web.js
@@ -6,15 +6,32 @@ var bodyParser  = require("body-parser"),
 
 var key = '19deda21-b9ca-40d3-af2c-5037a30b37b9';
 
+function promiseGet(url) {
+    return new Promise(function get(resolve, reject) {
+        request.get(url, function handleResp(err, resp, body) {
+            if (!err) {
+                resolve(body);
+            }
+            else {
+                reject(Error(err));
+            }
+        });
+    });
+}
+
+function promiseJsonGet(url) {
+    return promiseGet(url).then(JSON.parse);
+}
+
 var app = express();
 
 // Server defaults to port 7500
 app.set('port', (process.env.PORT || 5000));
 
 // Static serving files from specific folders
-// app.use('/css',         express.static(__dirname + '/css'));
-// app.use('/js',          express.static(__dirname + '/js'));
-// app.use('/images',      express.static(__dirname + '/images'));
+app.use('/css',         express.static(__dirname + '/css'));
+app.use('/js',          express.static(__dirname + '/js'));
+app.use('/images',      express.static(__dirname + '/images'));
 
 // Other stuff to use
 app.use(logfmt.requestLogger());
@@ -28,16 +45,26 @@ app.get('/', function(req, res) {
         type: 'RANKED_SOLO_5x5',
         api_key: key
     }
+
     var queryUrl = url + querystring.stringify(query);
 
-    request.get(queryUrl, function handleResp(err, resp, body) {
-        if (err) {
-            res.send(err);
-        }
-        else {
+    promiseJsonGet(queryUrl)
+        .then(function consolidateChallengerIds(json) {
+            challengerObjects = json.entries;
 
-        }
-    });
+            var challengerIds = [];
+
+            for (var key in challengerObjects) {
+                var id = challengerObjects[key].playerOrTeamId;
+
+                challengerIds.push(id);
+            }
+
+            return challengerIds;
+        })
+        .then(function getMatches(challengerIds) {
+            res.send(challengerIds);
+        });
 
 
     // var url = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?';
