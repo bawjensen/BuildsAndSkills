3ac681891fc5bc7d60592abfb749e97f4302f8d9
Bryan Jensen <bryanawjensen@gmail.com>
2015-02-09 14:30:59 -0500

    Small changes to looks and showing items

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(function() {
-    $('.collapsible').siblings(':has(h1)').find('h1').click(function() {
-        $(this).parent().siblings('.collapsible').slideToggle();
-    });
+    // $('.collapsible').siblings(':has(h1)').find('h1').click(function() {
+    //     $(this).parent().siblings('.collapsible').slideToggle();
+    // });
+
+    $('.display-all-items').click(function(evt) {
+        evt.preventDefault();
+        $('.item-buy.blacklisted').toggle();
+    })
 });
